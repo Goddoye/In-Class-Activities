ffbf9997f359313f4c15eea2048c286032584e2f
George Oddoye <50216284+Goddoye@users.noreply.github.com>
2019-08-19 20:44:51 -0400

    Add files via upload

---
+++ quick_python.py
@@ -0,0 +1,7 @@
+print("Python >VBA")
+my_name= input("What is your name? ")
+neighbor_name = input("What is your neighbors name? ")
+my_coding= input("How long have you been coding? ")
+neighbor_coding= input("How long has your neighbor been coding? ")
+print("My name is " +my_name+ " I have been coding for " + my_coding + ". " + "My neighbors name is " + neighbor_name+ " they have been coding for " + neighbor_coding+ ".")
+
