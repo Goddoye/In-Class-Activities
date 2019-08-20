170c35b732c04ba96ede4676558550833c04c0a5
George Oddoye <50216284+Goddoye@users.noreply.github.com>
2019-08-19 20:01:15 -0400

    Add files via upload

---
+++ Hello_Variable_World.py
@@ -0,0 +1,10 @@
+name = "George Oddoye"
+country = "USA"
+satisfied = True
+hourly_wage = 35
+age = 27
+daily_wage = (hourly_wage * 8)
+print(name + country + str(age) + str(hourly_wage))
+print(f" I make {daily_wage} per day")
+print(f" Are you satisfied with the GT Bootcamp? {satisfied}")
+
