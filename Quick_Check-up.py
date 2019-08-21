fddd4368abdefc5023da42e2741eab58791136ab
George Oddoye <50216284+Goddoye@users.noreply.github.com>
2019-08-21 18:58:50 -0400

    Add files via upload

---
+++ Quick_Check-up.py
@@ -0,0 +1,9 @@
+print("Hello User!")
+user= str(input("What is your name? "))
+print("Hello " + user)
+user_age=int((input("What is your age?")))
+
+if (user_age <= 25):
+	print("Awww... you're just a baby!")
+else:
+	print("Ah... A well traveled soul are ye")
