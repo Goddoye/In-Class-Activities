e3d7e982904ca6352879d7ad9b68880bd03bda53
George Oddoye <50216284+Goddoye@users.noreply.github.com>
2019-11-11 20:53:36 -0500

    JavaScript

---
+++ index.js
@@ -0,0 +1,15 @@
+var movie = {
+name : "Star Wars",
+year : 1997,
+profitable : true,
+sequels : [5,6,1,2,3, "The last Jedi"]
+}
+
+console.log(movie.name);
+console.log(movie.year);
+console.log(movie.sequels);
+
+
+movie.rating=9;
+
+console.log(movie.rating);
